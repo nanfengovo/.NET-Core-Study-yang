fe8d5e2f68ed0eac507bf3f978a782c20883d539
nanfengovo <nanfengovo@gmail.com>
2024-10-17 10:29:33 +0800

    异步编程

---
+++ 异步编程/awaitasync1/awaitasync1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace awaitasync1
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            #region 同步方法写入和读取文件
+            //string filename = @"E:\Test\1.txt";
+            //File.WriteAllText(filename,"hello");
+            //string s = File.ReadAllText(filename);
+            //Console.WriteLine(s);
+            #endregion
+
+            #region 异步方法写入和读取文件
+            string filename = @"E:\Test\2.txt";
+            await File.WriteAllTextAsync(filename, "hello");
+            string s = await File.ReadAllTextAsync(filename);
+            Console.WriteLine(s);
+            #endregion
+
+        }
+    }
+}
