8533bee4c18b337485331635395348acd8d9d5ec
nanfengovo <nanfengovo@gmail.com>
2024-12-20 21:02:59 +0800

    读取简单配置

---
+++ Part2 配置系统/Config 1/Config 1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace Config_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConfigurationBuilder configBuilder = new ConfigurationBuilder();
+            configBuilder.AddJsonFile("config.json",optional: true, reloadOnChange: true);
+            IConfigurationRoot configRoot  = configBuilder.Build();
+            string name = configRoot["name"];
+            Console.WriteLine("name:"+name);
+            string age = configRoot["age"];
+            Console.WriteLine("age:"+age);
+            string address = configRoot.GetSection("proxy:address").Value;
+            Console.WriteLine("address:"+address);
+        }
+    }
+}
